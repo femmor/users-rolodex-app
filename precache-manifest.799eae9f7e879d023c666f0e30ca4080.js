8a0a9ad5a99a7ff72bd6cc99117cbff95f93eb43
Emmanuel <femmor2u@gmail.com>
2019-09-11 14:26:24 -0500

    Updates

---
+++ precache-manifest.799eae9f7e879d023c666f0e30ca4080.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b617a9e936d2e9542d2c1ff6b7c4462",
+    "url": "/users-rolodex-app/index.html"
+  },
+  {
+    "revision": "3d9fe944fa2786bac532",
+    "url": "/users-rolodex-app/static/css/main.5f6f6a6d.chunk.css"
+  },
+  {
+    "revision": "f9544c9e233f918591b1",
+    "url": "/users-rolodex-app/static/js/2.e6ff8d7d.chunk.js"
+  },
+  {
+    "revision": "3d9fe944fa2786bac532",
+    "url": "/users-rolodex-app/static/js/main.f6fcfd3a.chunk.js"
+  },
+  {
+    "revision": "a1403ffb6746e8682ef3",
+    "url": "/users-rolodex-app/static/js/runtime~main.fd5da9cc.js"
+  }
+]);
